0e0f3577822c4a2e83760cd9d59657a3977a500a
fantasy995 <1642063542@qq.com>
2020-12-05 21:32:17 +0800

    与main分支合并

--- src/main/java/cn/hestyle/road_examination_manager/service/ICarService.java
+++ src/main/java/cn/hestyle/road_examination_manager/service/ICarService.java
@@ -10,11 +10,18 @@ import cn.hestyle.road_examination_manager.service.exception.InsertException;
  */
 public interface ICarService {
     /**
+     *新增车辆信息
      *
      * @param car 车辆信息
      * @return 新增车辆的数据
      */
     Car addNew(Car car) throws InsertException;
 
+    /**
+     * 根据id删除车辆数据
+     *
+     * @param id
+     * @throws CarNotFoundException
+     */
     void delById(Integer id) throws CarNotFoundException;
 }
