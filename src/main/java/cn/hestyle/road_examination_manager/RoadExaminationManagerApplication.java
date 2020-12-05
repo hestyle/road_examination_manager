dde6397e414f157dea0ca84efca1524bad0651aa
hestyle <hestyle@qq.com>
2020-12-05 15:21:07 +0800

    project first commit

---
+++ src/main/java/cn/hestyle/road_examination_manager/RoadExaminationManagerApplication.java
@@ -0,0 +1,13 @@
+package cn.hestyle.road_examination_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadExaminationManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadExaminationManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/hestyle/road_examination_manager/RoadExaminationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hestyle.road_examination_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadExaminationManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
